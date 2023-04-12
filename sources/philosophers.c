4dcc36e10c01e5d090c5d343fd5dbfd81e4b8782
Galo Joel <98602276+gjmacias@users.noreply.github.com>
2023-04-12 07:41:43 +0200

    Update philosophers.c

--- sources/philosophers.c
+++ sources/philosophers.c
@@ -1,1 +1,15 @@
 	
+#include "philo.h"
+
+int	main(int words, char **arguments)
+{
+	t_rules	rules;
+	int		result;
+	
+	if (words != 5 && words != 6)
+		return (write_error("Wrong inputs"));
+	if (result = init_philo(&rules, arguments))
+		return (error_manager(result));
+	if ()
+	return (0);
+}
