8ba07e8cb58ca97c7adb3855e1502e1a799d09bc
Galo Joel <98602276+gjmacias@users.noreply.github.com>
2023-04-12 07:18:09 +0200

    Update philosophers.h

--- sources/philosophers.h
+++ sources/philosophers.h
@@ -1,1 +1,22 @@
 
+
+typedef struct	s_philosopher
+{
+	int		id;
+	int		;
+	int		left_fork;
+	int		right_fork;
+	long long	;
+}	t_philosopher;
+
+typedef struct	s_rules
+{
+	int		n_philo;
+	int		death_time;
+	int		eat_time;
+	int		sleep_time;
+	int		;
+	int		;
+	long long	;
+	t_philosopher	philosophers[250];
+}	t_rules;
