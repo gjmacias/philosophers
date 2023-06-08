11a06ade50beb3d77564a7d31106d393bd7895df
Galo Joel <98602276+gjmacias@users.noreply.github.com>
2023-06-08 10:02:11 +0200

    Update philosophers.h

--- sources/philosophers.h
+++ sources/philosophers.h
@@ -1,4 +1,11 @@
 
+/*
+** Las estructura de filosofos guardan los enteros:
+** id(n, identificador), i_eat(Bool, si esta comiendo o no), Left(n) y Right(n)
+** guardando el identificador de los tenedores, last meal(Guarda el tiempo que vive)
+** Guardan el hilo y el proceso:
+** thread(identificador de hilo), proces(identificador de proceso)
+*/
 
 typedef struct	s_philosopher
 {
@@ -11,6 +18,14 @@ typedef struct	s_philosopher
 	pid_t		process_id;
 }	t_philosopher;
 
+/*
+** Las reglas guardan los enteros:
+** filosofos(n), T. muerte(ms), T. comer(ms), T. dormir(ms), muerto(Bool), comiendo(n)
+** Guardan los siguientes procesos de mutex:
+** , fork(posibles mutex), writing().
+** Y guardamos la estructura de filosofos, tantos como mutex haya.
+*/
+
 typedef struct	s_rules
 {
 	int		n_philo;
@@ -22,6 +37,6 @@ typedef struct	s_rules
 	long long int	first_time;
 	pthread_mutex_t	;
 	pthread_mutex_t	fork[250];
-	pthread_mutex_t	;
+	pthread_mutex_t	writing;
 	t_philosopher	philosophers[250];
 }	t_rules;
