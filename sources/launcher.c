cbb4f53d8bc5f57e55ef9532ef3269400cde34ff
Galo Joel <98602276+gjmacias@users.noreply.github.com>
2023-10-04 20:46:00 +0200

    Update launcher.c

--- sources/launcher.c
+++ sources/launcher.c
@@ -13,22 +13,33 @@ void	launcher(t_rules *r, t_philosopher *p)
 }
 
 /*
+** En la funcion thread solo podemos pasar un parametro, por ello añadimos rules a
+** cada filosofo.
+** 
+*/
+void	function_thread(void *void_p)
+{
+	
+}
+
+/*
 ** Creamos un puntero para recorrer todos lo filosofos, creando un hilo para cada uno
 ** de ellos. Tambien guarda el tiempo de creación tanto de las reglas como de cada filosofo
-** Una vez todo creado, ejecutamos el programa de los filosofos
+** genera un hilo para cada filosofo con la FUNCION_DEL_HILO(function_thread)
+** Una vez todo creado, ejecutamos el programa de los filosofos.
 */
 
 int	init_launcher(t_rules *rules)
 {
-	int	i;
+	int		i;
 	t_philosopher *p;
 
-	i = -1;
 	p = rule->philosophers;
 	rules->first_time = the_time();
+	i = -1;
 	while (++i < rules->n_philo)
 	{
-		if (pthread_create(&(p[i].thread_id), NULL, p_thread, &(p[i])))
+		if (pthread_create(&(p[i].thread_id), NULL, function_thread, &(p[i])))
 			return (1);
 		p[i].last_meal = the_time();
 	}
