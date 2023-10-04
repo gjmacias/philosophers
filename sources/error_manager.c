01c5f0274c08532f67bc38b52a0811ec5b8a2e67
Galo Joel <98602276+gjmacias@users.noreply.github.com>
2023-10-04 20:12:50 +0200

    Create error_manager.c

---
+++ sources/error_manager.c
@@ -0,0 +1,23 @@
+#include "philosophers.c"
+
+int	write_error(char *s)
+{
+	int len;
+
+	len = 0;
+	while (s[len])
+		len++;
+	write(2, "Error: ", 7);
+	write(2, s, len);
+	write(2, "\n", 1);
+	return (1);
+}
+
+int	error_manager(int error)
+{
+	if (error == 1)
+		return (write_error("At least, one wrong argument."));
+	if (error == 2)
+		return (write_error("Fatal error. When intializing mutex."));
+	return (1);
+}
